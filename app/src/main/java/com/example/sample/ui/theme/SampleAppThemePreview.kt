1531cde5a1462e7f12678073bb4cc70da322f977
arun-dh <arun.s@dedalus.eu>
2025-06-25 12:16:29 +0530

    added LocalNavController

--- app/src/main/java/com/example/sample/ui/theme/SampleAppThemePreview.kt
+++ app/src/main/java/com/example/sample/ui/theme/SampleAppThemePreview.kt
@@ -3,20 +3,27 @@ package com.example.sample.ui.theme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.unit.Density
+import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.testing.TestNavHostController
 import com.example.sample.ui.LocalNavController
 
 @Composable
 fun SampleAppThemePreview(content: @Composable () -> Unit) {
+    val navController = TestNavHostController(LocalContext.current).apply {
+        navigatorProvider.addNavigator(ComposeNavigator())
+    }
     SampleAppTheme {
         CompositionLocalProvider(
             LocalInspectionMode provides true,
             LocalDensity provides Density(
                 density = LocalDensity.current.density,
-            )
+            ),
+            LocalNavController provides navController
         ) {
             Surface {  content() }
         }
